43057d49f713214d844feb4e2599a17edb1d9eaf
anirhanane <79604511+anirhanane@users.noreply.github.com>
2022-06-26 20:32:50 +0200

    Update views.py

--- views.py
+++ views.py
@@ -5,7 +5,7 @@ import requests
 from flask import render_template, request, jsonify, make_response
 from werkzeug.utils import secure_filename
 from app import app
-from app.modeleML import ClassificationFF2, ClassificationFF4, nerExtraction
+from app.modeleML import ClassificationFF2, ClassificationFF4
 
 
 @app.route('/')
@@ -105,38 +105,3 @@ def classification2(path):
         response.headers["Content-Type"] = "application/json"
         return response
 
-
-@app.route('/api/extraction/<path>/', methods=['GET'])
-def extraction(path):
-    ents = []
-    if path != '':
-        path = path.replace(os.sep, '/')
-        filename = path.split('/')[-1]
-        try:
-            f = open(os.path.join('../' + app.config['UPLOAD_FOLDER'], filename), 'wb')
-            f.write(requests.get(path).content)
-            f.close()
-        except IOError as e:
-            print("I/O error({0})".format(e))
-        print(filename)
-        # filename = os.path.basename(path)
-        # destination = os.path.normpath(os.path.join('../' + app.config['UPLOAD_FOLDER'], filename))
-        # f.save(os.path.join('../' + app.config['UPLOAD_FOLDER'], filename))
-        monRepertoireimg = os.path.join('../' + app.config['UPLOAD_FOLDER'] + '/tmp')
-        # shutil.copyfile(path, destination)
-        entities = nerExtraction(os.path.join('../' + app.config['UPLOAD_FOLDER']) + '/' + filename,
-                                 monRepertoireimg)
-        for ent in entities:
-            ents.append([(ent.text, ent.label_)])
-        response = make_response(
-            jsonify(
-                {"entities": str(ents)}
-            ),
-            200,
-        )
-        if os.path.exists(os.path.join('../' + app.config['UPLOAD_FOLDER'], filename)):
-            os.remove(os.path.join('../' + app.config['UPLOAD_FOLDER'], filename))
-        else:
-            print("Impossible de supprimer le fichier car il n'existe pas")
-        response.headers["Content-Type"] = "application/json"
-        return response
